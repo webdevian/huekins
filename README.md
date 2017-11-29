89b643e3f21dfee70ec489c2fd0639612ef609fe
Ian Egner <ian.egner@apc-overnight.com>
2017-11-29 20:55:29 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# HueKins
+
+Connect your JenkinsCI server to a Philips Hue light. HueKins polls JenkinsCI job(s) for the lastBuild status and set the colour of a Philips Hue bulb accordingly
+
+## Installation
+
+Clone this repo and run `node index.js` to start the service
+
+## Configuration
+
+All configuration can be set up in a .env file:
+
+```yaml
+# The Address of your Philips Hue Bridge
+HUE_HOST=192.168.0.2
+
+# The port to access your Philips Hue Bridge
+HUE_PORT=80
+
+# your Philips Hue Bridge username
+# (See https://www.developers.meethue.com/documentation/getting-started)
+HUE_USERNAME= 
+
+# The timeout on requests to your Hue
+HUE_TIMEOUT=15000
+
+# The name of the Hue light to use
+LIGHT_NAME= 
+
+# The base url of your jenkins instance
+JENKINS_URL= 
+
+# Comma separated list of jenkins jobs to 
+# include. For github repos use the format 
+# of org_name/repo_name/branch_name
+JENKINS_JOBS= 
+```
---
+++ index.js
@@ -0,0 +1,137 @@
+const Huejay = require('huejay')
+require('dotenv').config()
+const { env } = process
+
+class HueKins {
+  constructor () {
+    this.hue = new Huejay.Client({
+      host: env.HUE_HOST,
+      port: env.HUE_PORT || 80,
+      username: env.HUE_USERNAME,
+      timeout: env.HUE_TIMEOUT || 15000
+    })
+
+    this.jenkins = require('jenkins')({ baseUrl: env.JENKINS_URL, promisify: true })
+
+    this.jobNames = env.JENKINS_JOBS.split(',')
+  }
+
+  async init () {
+    await this.getInfo()
+  }
+
+  async getInfo () {
+    const bridge = await this.hue.bridge.get()
+    console.log(`Retrieved bridge ${bridge.name}`)
+    console.log('  Id:', bridge.id)
+    console.log('  Model Id:', bridge.modelId)
+    console.log('  Model Name:', bridge.model.name)
+
+    this.bridge = bridge
+
+    const lights = await this.hue.lights.getAll()
+    console.log('Lights connected:')
+
+    lights.map(light => {
+      console.log(`  - ${light.attributes.attributes.name}`)
+    })
+
+    this.activeLight = lights.filter(light => light.attributes.attributes.name === env.LIGHT_NAME)[0]
+
+    this.lights = lights
+  }
+
+  flashLight () {
+    this.activeLight.alert = 'lselect'
+  }
+
+  async failingLight () {
+    this.flashLight()
+    this.activeLight.hue = 0
+    await this.hue.lights.save(this.activeLight)
+  }
+
+  async unstableLight () {
+    this.flashLight()
+    this.activeLight.hue = 25500 / 2
+    await this.hue.lights.save(this.activeLight)
+  }
+
+  async passingLight () {
+    this.activeLight.hue = 25500
+    await this.hue.lights.save(this.activeLight)
+  }
+
+  async buildingLight () {
+    this.flashLight()
+    this.activeLight.hue = 46920
+    await this.hue.lights.save(this.activeLight)
+  }
+
+  async getStatus () {
+    let building = false
+    let unstable = false
+    let failing = false
+
+    await Promise.all(this.jobNames.map(async jobName => {
+      const job = await this.jenkins.job.get(jobName)
+      const lastBuild = await this.jenkins.build.get(jobName, job.lastBuild.number)
+
+      if (lastBuild.result === 'FAILURE') {
+        failing = true
+      }
+
+      if (lastBuild.result === 'UNSTABLE' || lastBuild.result === 'ABORTED') {
+        unstable = true
+      }
+
+      if (!lastBuild.result) {
+        building = true
+      }
+
+      return lastBuild.result
+    }))
+
+    if (building) {
+      return 'building'
+    }
+
+    if (failing) {
+      return 'failing'
+    }
+
+    if (unstable) {
+      return 'unstable'
+    }
+
+    return 'passing'
+  }
+
+  async setStatus () {
+    const newStatus = await this.getStatus()
+    if (this.status !== newStatus) {
+      console.info(`Status has changed to ${newStatus}`)
+      this.status = newStatus
+      await this.setLight(newStatus)
+    } else {
+      return false
+    }
+  }
+
+  async setLight (status) {
+    await this[`${status}Light`]()
+  }
+
+  async startListener () {
+    await this.setStatus()
+    setInterval(await this.setStatus.bind(this), 2000)
+  }
+}
+
+const start = async () => {
+  const hue = new HueKins()
+  await hue.init()
+  await hue.startListener()
+}
+
+module.exports = start()
